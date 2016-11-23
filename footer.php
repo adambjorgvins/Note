a3dcc4df6e632e497cda22ea1a413f6de331cf19
Adam Bæhrenz Björgvinsson <adambjorgvinsson@gmail.com>
2016-11-23 22:44:58 +0000

    remove footer

--- footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: AdamB
- * Date: 23.11.2016
- * Time: 20:20
- */
-?>
-<p>This is the footer.</p>
--- master.php
+++ master.php
@@ -13,6 +13,5 @@
 <body>
     <div><?php include('menu.php');?></div>
     <div><?php include($page_content);?></div>
-    <div><?php include('footer.php');?></div>
 </body>
 </html>
